3e587328dc47b7b981371540f3b244d6ab6eb11c
nevermind <143873526+TlyaumbetovFS@users.noreply.github.com>
2026-04-24 13:47:41 +0300

    spring boot project initialization

---
+++ src/main/java/dev/lexsnap/LexSnapApplication.java
@@ -0,0 +1,11 @@
+package dev.lexsnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexSnapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LexSnapApplication.class, args);
+    }
+}
